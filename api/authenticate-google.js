1903353dff462973a045d8029415db98ae3c2e3f
Sailakshmi  Kumar <sailakshmikumaar@gmail.com>
2020-04-13 15:27:35 +0100

    SS: Fix for auth

--- api/authenticate-google.js
+++ api/authenticate-google.js
@@ -8,38 +8,38 @@ AWS.config.update({ region: 'us-east-2' });
 const jwtDecode = require('jwt-decode');
 const util = require('./util.js');
 
-const cognitoIdentity = new AWS.CognitoIdentity();
+const cognitoidentity = new AWS.CognitoIdentity();
 const identityPoolId = process.env.COGNITO_IDENTITY_POOL_ID;
 
 exports.handler = async (event) => {
     try {
         let id_token = util.getIdToken(event.headers);
+
         let params = {
-            identityPoolId: identityPoolId,
+            IdentityPoolId: identityPoolId,
             Logins: {
                 'accounts.google.com': id_token
-            }            
+            }
         };
 
-        let data = await cognitoIdentity.getId(params).promise();
+        let data = await cognitoidentity.getId(params).promise();
 
         params = {
             IdentityId: data.IdentityId,
             Logins: {
-                "accounts.google.com": id_token
+                'accounts.google.com': id_token
             }
         };
 
-        data = await cognitoIdentity.getCredentialsForIdentity(params).promise();
+        data = await cognitoidentity.getCredentialsForIdentity(params).promise();
         let decoded = jwtDecode(id_token);
         data.user_name = decoded.name;
-       
-            return {
-                statusCode: 200,
-                headers: util.getResponseHeaders(),
-                body: JSON.stringify(data)
-            };
-           
+
+        return {
+            statusCode: 200,
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify(data)
+        };
     } catch (err) {
         console.log("Error", err);
         return {
